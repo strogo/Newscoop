121fa296f1da7f09541a6324e7735893edbef62f
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2001-12-19 11:05:19 +0000

    - changed messages (dictionary -> infotype)

--- campsite/implementation/management/infotype/locals.ru.php
+++ campsite/implementation/management/infotype/locals.ru.php
@@ -1,62 +1,20 @@
 <?
-
-// -- index.m4
-regGS("Dictionary classes","Классы словаря:ru");
-regGS("Add new dictionary class","Добавить новый класс словаря:ru");
-regGS("Delete dictionary class $1","Удалить класс словаря $1:ru");
-regGS("No dictionary classes.","Нет классов словаря.:ru");
-
-
-// -- add.m4
-regGS("Add new class","Добавить класс словаря:ru");
-regGS("You do not have the right to add dictionary classes.","Вы не имеете права дообавлять классы словаря.:ru");
-
-
-// -- do_add.m4
-regGS("Adding new class","Добавление нового класса:ru");
-regGS("Adding new keyword class","Добавление нового класса ключевых слов:ru");
-regGS("You must complete the $1 field.","Вы должны заполнить поле $1.:ru");
-regGS("You must select a language.","Вы должны выбрать язык.:ru");
-regGS("The class  has been added.","Класс был добавлен.:ru");
-regGS("The class could not be added.","Не удалось добавить класс.:ru");
-regGS("Please check if the class does not already exist.","Пожалуйста проверьте, что класс еще не существует.:ru");
-// log
-regGS("Class $1 added","Класс $1 добавлен:ru");
-
-
-
-// -- del.m4
-regGS("Delete class","Удалить класс:ru");
-regGS("You do not have the right to delete dictionary classes.","Вы не имеете права удалять классы словаря.:ru");
-regGS("Are you sure you want to delete the class $1?","Вы уверены, что хотите удалить класс $1?:ru");
-regGS("No such class.","Нет такого класса.:ru");
-
-
-// -- do_del.m4
-regGS("Deleting class","Удаление класса:ru");
-regGS("The class has been deleted.","Класс был удален.:ru");
-regGS("The class could not be deleted.","Не удалось удалить класс.:ru");
-// log
-regGS("Class $1 deleted","Класс $1 удален:ru");
-
-
-// -- translate.m4
-regGS("Translate class","Перевести класс:ru");
-regGS("Translate keyword","Перевести ключевое слово:ru");
-regGS("No more languages.","Больше нет языков.:ru");
-regGS("No such keyword class.","Нет такого класса ключевых слов.:ru");
-regGS("",":ru");
-
-// -- do_translate.m4
-regGS("Adding new translation","Добавлеение нового перевода:ru");
-regGS("",":ru");
-regGS("",":ru");
-regGS("",":ru");
-regGS("",":ru");
-regGS("",":ru");
-
+regGS("Glossary infotypes","");
+regGS("Add new glossary infotype","");
+regGS("Delete glossary infotype $1","");
+regGS("No glossary infotypes.","");
+regGS("Add new infotype","");
+regGS("You do not have the right to add glossary infotypes.","");
+regGS("Adding new infotype","");
+regGS("The infotype  has been added.","");
+regGS("The infotype could not be added.","");
+regGS("Please check if the infotype does not already exist.","");
+regGS("Infotype $1 added","");
+regGS("Delete infotype","");
+regGS("You do not have the right to delete glossary infotypes.","");
+regGS("Are you sure you want to delete the infotype $1?","");
+regGS("Deleting infotype","");
+regGS("Infotype $1 deleted","");
+regGS("Translate infotype","");
 
 ?>
-
-
-

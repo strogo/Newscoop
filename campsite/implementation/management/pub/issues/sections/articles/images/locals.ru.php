a199014e9c809fbcd0d9612afa80e3ee59deb340
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2001-12-19 11:19:06 +0000

    *** empty log message ***

---
+++ campsite/implementation/management/pub/issues/sections/articles/images/locals.ru.php
@@ -0,0 +1,35 @@
+<?
+regGS("Add new image","");
+regGS("Select an old image","");
+regGS("Back to article details","");
+regGS("Click to view image","");
+regGS("Date<BR><SMALL>(yyyy-mm-dd)</SMALL>","");
+regGS("Delete image $1","");
+regGS("No images.","");
+regGS("View image","");
+regGS("Change image information","");
+regGS("Updating image information","");
+regGS("Edit image information","");
+regGS("Image information has been updated.","");
+regGS("Image information could not be updated.","");
+regGS("Changed image properties of $1","");
+regGS("Delete image","");
+regGS("You do not have the right to delete images","");
+regGS("Are you sure you want to delete the image $1?","");
+regGS("Deleting image","");
+regGS("The image $1 has been successfully deleted.","");
+regGS("The image $1 could not be deleted.","");
+regGS("Image $1 deleted","");
+regGS("Back to current article","");
+regGS("Add now","");
+regGS("Add to current article","");
+regGS("Adding new image","");
+regGS("You do not have the right to add images","");
+regGS("Adding new image...","");
+regGS("Image uploaded","");
+regGS("Selecting image","");
+regGS("The image $1 has been successfully added.","");
+regGS("Image $1 added","");
+regGS("The image $1 could not be added.","");
+
+?>

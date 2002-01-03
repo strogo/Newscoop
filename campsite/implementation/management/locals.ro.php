08eb87a49fef418c40b62d20c963d8ac4a5ad939
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2002-01-03 15:15:56 +0000

    *** empty log message ***

---
+++ campsite/implementation/management/locals.ro.php
@@ -0,0 +1,12 @@
+<?
+regGS("Please enter your user name and password","Introduce-ti numele de utilizator si parola");
+regGS("Access denied","Acces interzis");
+regGS("You do not have the right to access this page.","Nu aveti dreptul sa accesati aceasta pagina");
+regGS("Login failed","Eroare la deschiderea sesiunii");
+regGS("Pease make sure that you typed the correct user name and password.","");
+regGS("If your problem persists please contact the site administrator $1","");
+regGS("Name<BR><SMALL>(click to edit article)</SMALL>","Nume<br><font size=-1>(click pt. a edita articolul)</font>");
+regGS("Welcome $1!","");
+regGS("Localizer","");
+
+?>

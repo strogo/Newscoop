9c807c3d179fc005283d0098e5a02277da902ef7
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2002-01-03 19:32:08 +0000

    *** empty log message ***

---
+++ campsite/implementation/management/users/subscriptions/locals.ro.php
@@ -0,0 +1,32 @@
+<?
+regGS("Publication<BR><SMALL>(click to see sections)</SMALL>","");
+regGS("Left to pay","");
+regGS("Active","");
+regGS("Delete subscriptions to $1","");
+regGS("No subscriptions.","");
+regGS("Are you sure you want to delete the subscription to the publication $1?","");
+regGS("The subscription has been deleted.","");
+regGS("The subscription could not be deleted.","");
+regGS("Start","");
+regGS("Days","");
+regGS("(YYYY-MM-DD)","");
+regGS("Add sections now","");
+regGS("Add sections later","");
+regGS("The sections were added successfully.","");
+regGS("The subscription has been added successfully.","");
+regGS("The subscription could not be added.","");
+regGS("Please check if there isn't another subscription to the same publication.","");
+regGS("The sections could not be added successfully. Some of them were already added !","");
+regGS("Change subscription status","");
+regGS("ou do not have the right to change subscriptions status.","");
+regGS("Are you sure you want to deactivate the subscription?","");
+regGS("Are you sure you want to activate the subscription?","");
+regGS("Changing subscription status","");
+regGS("The subscription has been activated.","");
+regGS("The subscription has been deactivated.","");
+regGS("Subscription status could not be changed.","");
+regGS("Update payement","");
+regGS("The subscription payement was updated.","");
+regGS("Subscription payement could not be changed.","");
+
+?>

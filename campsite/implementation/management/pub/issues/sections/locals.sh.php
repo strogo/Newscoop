d03cc4b989983f7d0bdbb7cc80c64e3deea20c2d
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2002-05-06 13:51:20 +0000

    translation updates

--- campsite/implementation/management/pub/issues/sections/locals.sh.php
+++ campsite/implementation/management/pub/issues/sections/locals.sh.php
@@ -1,24 +1,26 @@
 <?
-regGS("Name<BR><SMALL>(click to see articles)</SMALL>","");
-regGS("Delete section $1","");
-regGS("No sections.","");
-regGS("Adding new section","");
-regGS("You do not have the right to add sections.","");
-regGS("The section $1 has been successfuly added.","");
-regGS("Please check if another section with the same number does not already exist.","");
-regGS("Section $1 added to issue $2. $3 ($4) of $5","");
-regGS("Delete section","");
-regGS("You do not have the right to delete sections.","");
-regGS("Are you sure you want to delete the section $1?","");
-regGS("Deleting section","");
-regGS("The section $1 has been deleted.","");
-regGS("Section $1 deleted from issue $2. $3 $4 of $5","");
-regGS("Change section name","");
-regGS("You do not have the right to change section details","");
-regGS("Updating section name","");
-regGS("The section $1 has been successfuly modified.","");
-regGS("The section could not be changed.","");
-regGS("Section $1 updated to issue $2. $3 ($4) of $5","");
-regGS("Name<BR><SMALL>(click to add article)</SMALL>","");
+
+regGS("Name<BR><SMALL>(click to see articles)</SMALL>","Ime<br><small>(kliknite da uðete u pojednaène èlanke)</small>");
+regGS("Delete section $1","Izbri¹i rubriku $1");
+regGS("No sections.","Nema nikakvih rubrika");
+regGS("Adding new section","Dodavanje nove rubrike");
+regGS("You do not have the right to add sections.","Nemate pravo da dodajete rubrike");
+regGS("The section $1 has been successfuly added.","Rubrika $1 je uspe¹no dodata.");
+regGS("Please check if another section with the same number does not already exist.","Proverite da li istoimena rubrika sa istim rednim brojem veæ ne postoji.");
+regGS("Section $1 added to issue $2. $3 ($4) of $5","Rubrika $1 je dodata u izdanje $2. $3 ($4) u $5");
+regGS("Delete section","Izbri¹i rubriku");
+regGS("You do not have the right to delete sections.","Nemate pravo da bri¹ete rubrike.");
+regGS("Are you sure you want to delete the section $1?","Da li ste sigurni da ¾elite da izbri¹ete rubriku $1?");
+regGS("Deleting section","Brisanje rubrike");
+regGS("The section $1 has been deleted.","Rubrika $1 je izbrisana");
+regGS("Section $1 deleted from issue $2. $3 $4 of $5","Rubrika $1 je izbrisana iz izdanja $2. $3 $4 u $5");
+regGS("Change section name","Promeni ime rubrike");
+regGS("You do not have the right to change section details","Nemate pravo da menjate detalje o rubrici");
+regGS("Updating section name","Promena imena rubrike");
+regGS("The section $1 has been successfuly modified.","Rubrika $1 je uspe¹no izmenjena.");
+regGS("The section could not be changed.","Rubrika nije mogla da bude izmenjena.");
+regGS("Section $1 updated to issue $2. $3 ($4) of $5","Rubrika $1 je izmenjena u izdanju 2. $3 ($4) u $5");
+regGS("Name<BR><SMALL>(click to add article)</SMALL>","Ime<br><small>(kliknite da dodate èlanak)</small>");
+
 
 ?>

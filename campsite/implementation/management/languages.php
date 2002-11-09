6c2a2b74db753f3808e48754aa070613e6b02e62
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2002-11-09 20:23:48 +0000

    removed serbo-croatian and added croatian

--- campsite/implementation/management/languages.php
+++ campsite/implementation/management/languages.php
@@ -11,6 +11,6 @@ registerLanguage('Russian','ru','ISO-8859-5');
 registerLanguage('German','de','ISO-8859-1');
 registerLanguage('Czech','cz','ISO-8859-2');
 registerLanguage('Romanian','ro','ISO-8859-2');
-registerLanguage('Serbo-Croatian','sh','ISO-8859-2');
+registerLanguage('Croatian','hr','ISO-8859-2');
 
 ?>

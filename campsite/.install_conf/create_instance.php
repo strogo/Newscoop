f2e300ced2298fa5522f3b6f11b9750e308a4b77
(no author) <(no author)@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-04-20 18:38:39 +0000

    This commit was manufactured by cvs2svn to create tag 'CAMPSITE_2_2_1_RC1'.

--- campsite/.install_conf/create_instance.php
+++ campsite/.install_conf/create_instance.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!is_array($GLOBALS['argv'])) {
+	echo "Can't read command line arguments\n";
+	exit(1);
+}
+
 global $Campsite, $CampsiteVars, $CampsiteOld, $info_messages;
 $Campsite = array();
 $CampsiteVars = array();
@@ -435,8 +440,8 @@ function create_virtual_host(&$p_defined_parameters)
 	fwrite($res, $new_file_content);
 	fclose($res);
 
-	$info_messages[] = "The apache virtual host configuration file $instance_vhost was created.";
-	$info_messages[] = "Please edit it and replace \$SERVER_ADDRESS and \$SERVER_NAME with appropriate values.";
+	$info_messages[] = "The apache virtual host configuration file:\n\t$instance_vhost\nwas created.";
+	$info_messages[] = "Please edit it and replace \$SERVER_ADDRESS and \$SERVER_NAME with\nappropriate values.";
 
 	return 0;
 }

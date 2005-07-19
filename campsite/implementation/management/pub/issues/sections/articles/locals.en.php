3234b7707c4fba84b234442b2cd0570ed3718af1
mugur <mugur@952d13e4-84fd-0310-bd7c-ee2827c68691>
2005-07-19 12:55:13 +0000

    added messages related to bug #1214: No article types should cause alert before add new article form

--- campsite/implementation/management/pub/issues/sections/articles/locals.en.php
+++ campsite/implementation/management/pub/issues/sections/articles/locals.en.php
@@ -61,5 +61,7 @@ regGS("Duplicate article", "Duplicate article");
 regGS("The destination section is the same as the source section.", "The destination section is the same as the source section.:en");
 
 regGS("The article is new; it is not possible to schedule it for automatic publishing.", "The article is new; it is not possible to schedule it for automatic publishing.:en");
+regGS("No article types were defined. You must create an article type first.", "No article types were defined. You must create an article type first.:en");
+regGS("Edit article types", "Edit article types:en");
 
 ?>

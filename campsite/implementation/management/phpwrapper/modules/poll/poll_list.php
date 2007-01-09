41ed9cb46713fdefedcc827e0d7fdf73df460a75
sebastian <sebastian@952d13e4-84fd-0310-bd7c-ee2827c68691>
2007-01-09 15:38:57 +0000

    poll_list localized template support

--- campsite/implementation/management/phpwrapper/modules/poll/poll_list.php
+++ campsite/implementation/management/phpwrapper/modules/poll/poll_list.php
@@ -2,5 +2,11 @@
 require_once 'poll_list.class.php';
 
 new poll_list2smarty(getCampParametersInt(), $PARAMS['STATEMENT_PARAMS'], $REQUEST['poll'], &$Smarty);
-$Smarty->display('poll_list.tpl');
+$localized_template = 'poll_list_'.getCampParameters('Language Code').'.tpl';
+
+if (file_exists($Smarty->template_dir.$localized_template)) {
+    $Smarty->display($localized_template);
+} else {
+    $Smarty->display('poll_default.tpl'); 
+}
 ?>

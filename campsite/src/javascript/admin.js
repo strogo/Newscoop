54b25297f35be4e728f5f5ecd0aaedd4891e8e1a
petr.jasek <petr.jasek@sourcefabric.org>
2010-10-21 10:31:57 +0200

    update to cs-2277 - search for article topics popup

--- campsite/src/javascript/admin.js
+++ campsite/src/javascript/admin.js
@@ -22,14 +22,23 @@ function flashMessage(message, type)
         .fadeOut('slow');
 }
 
+var autocomplete = false;
+var terms = [];
 $(document).ready(function() {
-    var terms = [];
-    $('ul.tree.sortable strong').each(function() {
-        terms.push($(this).text());
-    });
-
+    // topics autocomplete
     $('.autocomplete.topics').autocomplete({
         source: function(request, response) {
+            if (terms.length == 0) { // populate terms
+                $('ul.tree.sortable strong').each(function() {
+                    terms.push($(this).text());
+                });
+            }
+            if (terms.length == 0) { // still needs to populate
+                $('ul.tree label').each(function() {
+                    terms.push($(this).text());
+                });
+            }
+
             var match = [];
             var re = new RegExp(request.term, "i");
             for (i = 0; i < terms.length; i++) {
@@ -38,6 +47,9 @@ $(document).ready(function() {
                 }
             }
             response(match);
-        }
+        },
+        close: function(event, ui) {
+            $('input[name=search]').change();
+        },
     });
 });

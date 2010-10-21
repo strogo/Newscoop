3f6887f645351989f5513da2af3605deb8014741
petr.jasek <petr.jasek@sourcefabric.org>
2010-10-21 09:50:56 +0200

    update to cs-2277 - search autocomplete for topics admin

--- campsite/src/javascript/admin.js
+++ campsite/src/javascript/admin.js
@@ -21,3 +21,23 @@ function flashMessage(message, type)
         .delay(3000)
         .fadeOut('slow');
 }
+
+$(document).ready(function() {
+    var terms = [];
+    $('ul.tree.sortable strong').each(function() {
+        terms.push($(this).text());
+    });
+
+    $('.autocomplete.topics').autocomplete({
+        source: function(request, response) {
+            var match = [];
+            var re = new RegExp(request.term, "i");
+            for (i = 0; i < terms.length; i++) {
+                if (terms[i].search(re) >= 0) {
+                    match.push(terms[i]);
+                }
+            }
+            response(match);
+        }
+    });
+});

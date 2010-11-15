327bf60da668ca917e2ab64815853a8f230142ee
petr.jasek <petr.jasek@sourcefabric.org>
2010-11-15 09:21:45 +0100

    update to CS-2218 - update table names

--- campsite/src/install/sql/upgrade/3.4.x/tables.sql
+++ campsite/src/install/sql/upgrade/3.4.x/tables.sql
@@ -57,8 +57,8 @@ CREATE TABLE `AuthorsAuthorsTypes` (
 
 ALTER TABLE `liveuser_users` ADD COLUMN `password_reset_token` VARCHAR(85) NULL  AFTER `isActive` ;
 
-DROP TABLE IF EXISTS `widget`;
-CREATE TABLE IF NOT EXISTS `widget` (
+DROP TABLE IF EXISTS `Widget`;
+CREATE TABLE IF NOT EXISTS `Widget` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `path` varchar(255) NOT NULL DEFAULT '',
   `class` varchar(78) NOT NULL DEFAULT '',
@@ -66,16 +66,16 @@ CREATE TABLE IF NOT EXISTS `widget` (
   UNIQUE KEY (`path`, `class`)
 );
 
-DROP TABLE IF EXISTS `widgetcontext`;
-CREATE TABLE IF NOT EXISTS `widgetcontext` (
+DROP TABLE IF EXISTS `WidgetContext`;
+CREATE TABLE IF NOT EXISTS `WidgetContext` (
   `id` smallint(3) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(80) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 );
 
-DROP TABLE IF EXISTS `widgetcontext_widget`;
-CREATE TABLE IF NOT EXISTS `widgetcontext_widget` (
+DROP TABLE IF EXISTS `WidgetContext_Widget`;
+CREATE TABLE IF NOT EXISTS `WidgetContext_Widget` (
   `fk_widgetcontext_id` smallint(3) unsigned NOT NULL,
   `fk_widget_id` mediumint(8) unsigned NOT NULL,
   `fk_user_id` int(10) unsigned NOT NULL,
